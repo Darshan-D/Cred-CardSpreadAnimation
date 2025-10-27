9a19ed261e7ddd20bcba578ecf83de360221ccbf
Darshan Dodia <58351607+Darshan-D@users.noreply.github.com>
2025-10-27 22:44:43 +0530

    Add files via upload

---
+++ credAnimation/credAnimationApp.swift
@@ -0,0 +1,17 @@
+////
+////  credAnimationApp.swift
+////  credAnimation
+////
+////  Created by Darshan Dodia on 19/06/25.
+////
+
+import SwiftUI
+
+@main
+struct credAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DraggableCardStackView()
+        }
+    }
+}
